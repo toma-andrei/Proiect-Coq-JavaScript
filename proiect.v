c69b0faf9a0811badc7d1dfc0f712a21c2f40930
toma-andrei <58333031+toma-andrei@users.noreply.github.com>
2021-01-11 15:40:59 +0200

    Add files via upload

--- proiect.v
+++ proiect.v
@@ -13,6 +13,7 @@ Inductive Exp :=
 | plus : Exp -> Exp -> Exp
 | times : Exp -> Exp -> Exp
 | div : Exp -> Exp -> Exp (**)
+| mod : Exp -> Exp -> Exp
 | minus : Exp -> Exp -> Exp
 | true : Exp
 | false : Exp
@@ -32,6 +33,7 @@ Notation "A +' B" := (plus A B) (at level 50).
 Notation "A -' B" := (minus A B) (at level 50).
 Notation "A *' B" := (times A B) (at level 46).
 Notation "A /' B" := (div A B) (at level 46).
+Notation "A %' B" := (mod A B) (at level 46).
 Notation "A <' B" := (blessthan A B) (at level 53).
 Notation "A <=' B" := (blesseqthan A B) (at level 53).
 Notation "A >' B" := (bgreaterthan A B) (at level 53).
@@ -39,7 +41,7 @@ Notation "A >=' B" := (bgreatereqthan A B) (at level 53).
 Notation "! A" := (not A) (at level 52).
 Notation "A &&' B" := (and A B) (at level 60).
 Notation "A ||' B" := (or A B) (at level 61).
-Notation "A == B" := (beqto A B) (at level 62).
+Notation "A ==' B" := (beqto A B) (at level 62).
 Notation "A != B" := (notbeqto A B) (at level 62).
 (*Notation "A === B" := (eqValeqType A B) (at level 62).
 Notation "A !== B" := (noteqValeqType A B) (at level 62).*)
@@ -50,7 +52,7 @@ Coercion evar : identif >-> Exp.
 
 Compute (true +' 1).
 Compute (true +' true).
-Compute (true == 1).
+Compute (true ==' 1).
 Compute (' "str" ').
 Compute (1 <' 2).
 
@@ -122,6 +124,18 @@ Inductive exp_eval : Exp -> Env -> nat -> Prop :=
   a1 =[ sigma ]=> i1 ->
   a2 =[ sigma ]=> 0 ->
   a1 /' a2 =[ sigma ]=> 0
+| mod_not_0 : forall a1 a2 i1 i2 sigma result,
+  a1 =[ sigma ]=> i1 ->
+  a2 =[ sigma ]=> i2 ->
+  result = Nat.modulo i1 i2 ->
+  a1 %' a2 =[ sigma ]=> result
+| notEval : forall a1 i1 state,
+  a1 =[ state ]=> i1 ->
+  !a1 =[ state ]=> if(Nat.eqb i1 0) then 1 else 0
+| mod_0 : forall a1 a2 i1 sigma,
+  a1 =[ sigma ]=> i1 ->
+  a2 =[ sigma ]=> 0 ->
+  a1 %' a2 =[ sigma ]=> 0
 | minusEval : forall a1 a2 i1 i2 sigma result,
   a1 =[ sigma ]=> i1 ->
   a2 =[ sigma ]=> i2 ->
@@ -150,34 +164,29 @@ Inductive exp_eval : Exp -> Env -> nat -> Prop :=
   a1 =[ state ]=> i1 ->
   a2 =[ state ]=> i2 ->
   comp = Nat.eqb i1 i2 ->
-  (beqto a1 a2) =[ state ]=> if (comp) then 1 else 0
+  (a1 ==' a2) =[ state ]=> if (comp) then 1 else 0
 | neqtoEval : forall a1 a2 i1 i2 state comp, 
   a1 =[ state ]=> i1 -> 
   a2 =[ state ]=> i2 ->
   comp = Nat.eqb i1 i2 ->
   (notbeqto a1 a2) =[ state ]=> if (comp) then 0 else 1
+| andEval_true : forall a1 a2 state boolean,
+  a1 =[ state ]=> 1 ->
+  a2 =[ state ]=> boolean ->
+  a1 &&' a2 =[ state ]=> boolean 
+| andEval_false : forall a1 a2 state boolean,
+  a1 =[ state ]=> 0 ->
+  a1 &&' a2 =[ state ]=> boolean
+| orEval_true : forall a1 a2 state,
+  a1 =[ state ]=> 1 ->
+  (a1 ||' a2) =[ state ]=> 1
+| orEval_false : forall a1 a2 state boolean,
+  a1 =[ state ]=> 0 ->
+  a2 =[ state ]=> boolean ->
+  (a1 ||' a2) =[ state ]=> boolean
 where "a =[ s ]=> n" := (exp_eval a s n).
 
-Require Import Omega.
 
-Example e1 : 3 +' a *' 2 =[ envA ]=> 5.
-Proof.
-  eapply add.
-  eapply const.
-  eapply timesEval.
-  eapply var.
-  eapply const.
-  eauto.
-  eauto.
-Qed.
-
-Example e2 : 1 +' true =[ envA ]=> 2.
-Proof.
-  eapply add.
-  eapply const.
-  eapply trueEval.
-  omega.
-Qed.
 
 Reserved Notation " S ={ state }=> state'" (at level 60).
 Inductive Stmt_eval : Statement -> Env -> Env -> Prop :=
@@ -206,17 +215,49 @@ Inductive Stmt_eval : Statement -> Env -> Env -> Prop :=
     While cond s1 ={ state }=> state
 where "s ={ state }=> state'" := (Stmt_eval s state state').
 
+Require Import Omega.
+
+Example e1 : 3 +' a *' ( 2 %' 2) =[ envA ]=> 3.
+Proof.
+  eapply add.
+  eapply const.
+  eapply timesEval.
+  eapply var.
+  eapply mod_not_0.
+  eapply const.
+  eapply const.
+  eauto.
+  eauto.
+  omega.
+Qed.
+
+Example e2 : b +' (!((true ==' false))) =[ envA ]=> 11.
+Proof.
+  eapply add.
+  eapply var.
+  eapply notEval.
+  eapply eqtoEval.
+  eapply trueEval.
+  eapply falseEval.
+  simpl.
+  eauto.
+  simpl.
+  reflexivity.
+Qed.
+
 Definition program :=
-  a =' 2 +' true ;
-  If ( a == 2 ) { a =' 4 } Else { a =' 5 } ;
-  c =' 4;
+  a =' 2 +' (! false) ;
+
+  If ( a ==' 2 ) { a =' 4 } Else { a =' 5 } ;
   
-  while ( a <' c ) { a =' a +' 1 }  
+  while ( (a %' 2) ==' 1 ) { 
+              a =' a +' 1 
+                          }  
 .
 
 Definition stateFirst := fun (x : identif) => 0.
 
-Example evalProgram : exists state, program ={ stateFirst }=> state /\ state a = 4.
+Example evalProgram : exists state, program ={ stateFirst }=> state /\ state a = 6.
 Proof.
   eexists.
   split.
@@ -225,15 +266,14 @@ Proof.
   eapply assign_eval.
   eapply add. 
   eapply const.
+  eapply notEval.
+  eapply falseEval.
   eauto.
-  eapply trueEval.
   eauto.
-  eauto.
-  eapply seq_eval.
   eapply seq_eval.  
   eapply if_false_eval.
   simpl.
-
+  eapply eqtoEval.
 Abort.
   
   
